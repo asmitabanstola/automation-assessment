c94a2e2e6ba01502131678540fb9d6101ea4aa0a
Asmita Banstola <banstolaasmita1@gmail.com>
2025-07-28 22:28:08 +0545

    added invalid assertion for login

--- Tests/login_test.py
+++ Tests/login_test.py
@@ -39,6 +39,39 @@ def test_login():
     finally:
         driver.quit()
 
+
+def test_invalid_login():
+    chrome_options = Options()
+    chrome_options.add_argument('--start-maximized')
+    chrome_options.page_load_strategy = 'eager'
+
+    service = Service('/usr/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    try:
+        login_page = LoginPage(driver)
+        login_page.open_login_page()
+
+        # Use invalid username and password
+        invalid_username = "test@example.com"
+        invalid_password = "password123"
+
+        login_page.enter_username(invalid_username)
+        login_page.enter_password(invalid_password)
+        login_page.click_login()
+
+        try:
+            toast = WebDriverWait(driver, 3).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "iweb-toast-wrap"))
+            )
+            if "Invalid account or password" in toast.text:
+                print(" Login failed — invalid credentials.")
+        except:
+            print("⚠️ No toast message appeared (maybe login succeeded or toast timed out).")
+
+    finally:
+        driver.quit()
+
 if __name__ == '__main__':
-    test_login()
-vvv
+    test_login() # valid credential
+    test_invalid_login()  # Test invalid credentials
